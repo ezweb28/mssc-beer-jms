2d43874fec02d958aa51f5ff4e288e7552869b28
Sunpil Kim <ezweb28@gmail.com>
2023-03-02 22:38:10 -0500

    Initialized Project and Maven Dependencies

---
+++ src/main/java/skim/springframework/msscbeerjms/MsscBeerJmsApplication.java
@@ -0,0 +1,13 @@
+package skim.springframework.msscbeerjms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBeerJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBeerJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/skim/springframework/msscbeerjms/MsscBeerJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package skim.springframework.msscbeerjms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
